af07f4d96188d24498f56123de74071ec74b1ee9
Emzzy241 <emzzyoluwole@gmail.com>
2025-04-30 01:35:46 +0100

    add entry point for CLI tool

---
+++ bin/gh-tracker.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+// This is the entry point for our CLI tool
+// The shebang line above tells Unix-like systems to run this file with Node.js
+
+const cli = require('../src/cli');
+cli.run();
+
