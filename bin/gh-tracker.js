210cb0d461d5fdc4eea4057bb6bf6c6bfba21506
Emzzy241 <emzzyoluwole@gmail.com>
2025-05-11 18:56:56 +0100

    add CLI tool entry point

--- bin/gh-tracker.js
+++ bin/gh-tracker.js
@@ -2,6 +2,7 @@
 
 // This is the entry point for our CLI tool
 // The shebang line above tells Unix-like systems to run this file with Node.js
+// This actually helps in ensuring that my CLI tool is cross-platform, in the sense that it works well even while using other OS like Linux, 
 
 const cli = require('../src/cli');
 cli.run();
